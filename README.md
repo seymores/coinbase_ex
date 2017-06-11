6ddfce89fc63cef56225611d205d481a415eca22
Ping <seymores@gmail.com>
2017-06-11 19:52:26 +0800

    Add some doc

--- README.md
+++ README.md
@@ -1,6 +1,34 @@
-# Coinbase
+# coinbase_ex
 
-**TODO: Add description**
+An easy way to buy, send, and accept bitcoin through the [Coinbase API](https://developers.coinbase.com).  
+This library supports both the [API key authentication](https://developers.coinbase.com/docs/wallet/api-key-authentication) method only for now.
+
+## Usage
+
+### Market Data
+
+Get supported native currencies
+
+```
+Coinbase.get_currencies()
+```
+
+Get buy price
+
+```
+Coinbase.get_buy_price("BTC-MYR")
+```
+
+
+## Configuration
+Please configure your API key and secret in your config file, see dev.secret.exs_sample.
+
+```
+config :coinbase, api_key: "YOUR_API_KEY",
+                  api_secret: "YOUR_API_SECRET",
+                  api_url: "https://api.coinbase.com",
+                  version: "2016-08-10"
+```
 
 ## Installation
 
---
+++ lib/http.ex
@@ -0,0 +1,78 @@
+defmodule Coinbase.Http do
+
+    def update!(url_path, body) when is_map(body) do
+      case update(url_path, body) do
+        {:error, error} -> error.reason
+        {:ok, %{status_code: 200} = response} -> response.body |> Poison.decode!()
+        {_, msg} -> msg.body |> Poison.decode!()
+      end
+    end
+
+    def update(url_path, body) when is_map(body) do
+      body = Poison.encode!(body)
+      {url, headers, options} = build_request("PUT", url_path, body)
+      HTTPoison.put(url, body, headers, options)
+    end
+
+    def delete(url_path) do
+      {url, headers, options} = build_request("DELETE", url_path)
+      HTTPoison.delete(url, headers, options)
+    end
+
+    def post!(url_path, body) when is_map(body) do
+      case post(url_path, body) do
+        {:error, error} -> error.reason
+        {:ok, %{status_code: 200} = response} -> response.body |> Poison.decode!()
+        {_, msg} -> msg.body |> Poison.decode!()
+      end
+    end
+
+    def post(url_path, body) when is_map(body) do
+      body = Poison.encode!(body)
+      {url, headers, options} = build_request("POST", url_path, body)
+      HTTPoison.post(url, body, headers, options)
+    end
+
+  @doc """
+    Construct generic GET request with specified URL path.
+    Returns full HTTPoison response.
+  """
+  def get(url_path) do
+    {url, headers, options} = build_request("GET", url_path)
+    HTTPoison.get(url, headers, options)
+  end
+
+  @doc """
+    Construct generic GET request with specified URL path.
+    Returns response body only.
+  """
+  def get!(url_path) do
+    case get(url_path) do
+      {:error, error} -> error.reason
+      {:ok, %{status_code: 200} = response} -> response.body |> Poison.decode!()
+      {_, msg} -> msg.body |> Poison.decode!()
+    end
+  end
+
+  @doc """
+    Handles the signing and configuring the headers with required properties like
+    CB-ACCESS-KEY, CB-ACCESS-SIGN, CB-VERSION, and CB-ACCESS-TIMESTAMP.
+    CB-VERSION value set in configuration.
+    See https://developers.coinbase.com/docs/wallet/api-key-authentication.
+  """
+  defp build_request(method, url_path, body \\ "") do
+    key     = Application.get_env(:coinbase, :api_key)
+    secret  = Application.get_env(:coinbase, :api_secret)
+    version = Application.get_env(:coinbase, :version)
+    ts      = inspect(:os.system_time(:seconds))
+    msg     = "#{ts}#{method}#{url_path}#{body}"
+    sign = :crypto.hmac(:sha256, secret, msg) |> Base.encode16(case: :lower)
+    headers = [{"Content-Type", "application/json"},
+               {"CB-ACCESS-KEY", key}, {"CB-ACCESS-SIGN", sign},
+               {"CB-ACCESS-TIMESTAMP", ts}, {"CB-VERSION", version}]
+    options = [ssl: [{:versions, [:'tlsv1.2']}], recv_timeout: 5000]
+    url     = Application.get_env(:coinbase, :api_url) <> url_path
+    {url, headers, options}
+  end
+
+end
